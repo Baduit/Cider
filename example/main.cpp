fb29f250aa30d47b32c869fafe94a60639026b86
Lena Bertho <lena.bertho@protonmail.com>
2023-03-18 22:00:58 +0100

    🎉: Basic setup of the project

---
+++ example/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include <cider/cider.hpp>
+
+int main()
+{
+	std::cout << "Le cidre breton est meilleur que le cidre normand." << std::endl;
+}
---
+++ lib/cider/cider.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace cider 
+{
+
+} // namespace cider 

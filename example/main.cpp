b5a1c8d3c0957605e06a5da4e2b8c81bf22f0757
Lena Bertho <lena.bertho@protonmail.com>
2023-03-19 16:29:27 +0100

    ✅: static_assert for helper and example

--- example/main.cpp
+++ example/main.cpp
@@ -2,7 +2,45 @@
 
 #include <cider/cider.hpp>
 
+using cider::helper::ClassType;
+using cider::operator->*;
+using cider::Wrapper;
+
+struct Foo
+{
+	void bar(int a, int b)
+	{
+		std::cout << (a + b) << std::endl;
+	};
+};
+
+static_assert(std::same_as<decltype(&Foo::bar), void (Foo::*)(int, int)>);
+static_assert(std::same_as<ClassType<decltype(&Foo::bar)>, Foo>);
+
 int main()
 {
+	// Both example make the exact same thing
+	{
+		auto wrapped = Wrapper(&Foo::bar);
+		Foo foo;
+		foo->*wrapped(1, 1);
+	}
+	{
+		auto member_function = &Foo::bar;
+		Foo foo;
+		(foo.*member_function)(1, 1);
+	}
+	// You don't have to use auto if you don't want to
+	{
+		Wrapper<void (Foo::*)(int, int)> wrapped = Wrapper(&Foo::bar);
+		Foo foo;
+		foo->*wrapped(1, 1);
+	}
+	{
+		void (Foo::*member_function)(int, int) = &Foo::bar;
+		Foo foo;
+		(foo.*member_function)(1, 1);
+	}
+
 	std::cout << "Le cidre breton est meilleur que le cidre normand." << std::endl;
 }
